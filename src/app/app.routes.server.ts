a7e53d6242b72b917ad0eb9d7f4f1ffc0069b40e
bagasaditya <bagasadit78@gmail.com>
2026-09-16 21:40:04 +0700

    fix: angular server route

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,34 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'products/details/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'subject/products/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'pengajuan-pinjaman/:id/review',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'pengajuan-pinjaman/:id/approval',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'pengajuan-pinjaman/:id/disbursement',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'customers/detail/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'limit-setting/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
