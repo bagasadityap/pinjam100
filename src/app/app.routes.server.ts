f57f32694c581d0a004c47f7dbd5085dcb6a38a7
bagasaditya <bagasadit78@gmail.com>
2026-09-16 21:41:35 +0700

    fix: angular server route typo

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,14 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'products/details/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'subject/products/:id',
-    renderMode: RenderMode.Server
-  },
-  {
     path: 'pengajuan-pinjaman/:id/review',
     renderMode: RenderMode.Server
   },
