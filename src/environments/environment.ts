76b462d8b603b0025574495a56038f6ee61f6097
bagasaditya <bagasadit78@gmail.com>
2026-09-23 00:27:10 +0700

    fix: error

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
     version: '1.0.0'
   },
   api: {
-    baseUrl: 'http://localhost:8080/api',
+    baseUrl: 'https://api.pinjam100.bagasaditya.com/api',
     documentUrl: 'https://api.pinjam100.bagasaditya.com/api/uploads/files',
     wilayahUrl: 'https://wilayah.id/api',
     timeout: 30000
