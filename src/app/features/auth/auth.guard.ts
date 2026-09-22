84907efcb91bc4095b8fb9668eeeb04512a91c7d
bagasaditya <bagasadit78@gmail.com>
2026-09-23 00:43:37 +0700

    fix: render mode prerender and fix authguard

--- src/app/features/auth/auth.guard.ts
+++ src/app/features/auth/auth.guard.ts
@@ -1,11 +1,17 @@
-import { inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 import { TokenService } from '../../core/service/token.service';
 
 export const authGuard: CanActivateFn = () => {
+  const platformId = inject(PLATFORM_ID);
   const tokenService = inject(TokenService);
   const router = inject(Router);
+
+  if (!isPlatformBrowser(platformId)) {
+    return true;
+  }
 
   const token = tokenService.get();
 
