0b29efd576fda3ce6f14fb9069145a2af4d2e47f
bagasaditya <bagasadit78@gmail.com>
2026-09-23 08:52:51 +0700

    fix: role guard support ssr

--- src/app/core/guard/role.guard.ts
+++ src/app/core/guard/role.guard.ts
@@ -1,10 +1,17 @@
-import { inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateChildFn, Router } from '@angular/router';
+
 import { AuthStateService } from '../service/auth-state.service';
 
 export const roleGuard: CanActivateChildFn = (route) => {
+  const platformId = inject(PLATFORM_ID);
   const authState = inject(AuthStateService);
   const router = inject(Router);
+
+  if (!isPlatformBrowser(platformId)) {
+    return true;
+  }
 
   const requiredRoles = route.data['roles'] as string[];
   const currentRole = authState.getRole();
